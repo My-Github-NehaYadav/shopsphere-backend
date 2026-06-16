76df922513b00a96a911225fdf9bbb8c947cb49d
Neha Yadav <96839342+My-Github-NehaYadav@users.noreply.github.com>
2026-06-16 15:53:04 +0530

    complete shopsphere crud

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -168,9 +168,100 @@ const getProducts = async (req, res) => {
   }
 };
 
+const getProductById = async (req, res) => {
+  const _id = req.params.id;
+  try {
+    const product = await Product.findById(_id);
+
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Products fetched successfully",
+      data: product,
+    });
+  } catch (error) {
+    console.log("Get Products Error:", error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+}
+
+
+const deleteProduct = async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const product = await Product.findByIdAndDelete(_id);
+
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Product deleted successfully",
+    });
+  } catch (error) {
+    console.log("Delete Product Error:", error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+};
+
+const updateProduct = async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const product = await Product.findByIdAndUpdate(
+      _id,
+      req.body,
+      { new: true }
+    );
+
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Product updated successfully",
+      data: product,
+    });
+
+  } catch (error) {
+    console.log("Update Product Error:", error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+};
+
 module.exports = {
   signup,
   login,
   createProduct,
-  getProducts
+  getProducts,
+  getProductById,
+  deleteProduct,
+  updateProduct
 };
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { signup, login, createProduct, getProducts } = require("../controllers/user.controller");
+const { signup, login, createProduct, getProducts, getProductById, deleteProduct, updateProduct } = require("../controllers/user.controller");
 
 router.post("/signup", signup);
 router.post('/login', login);
@@ -9,5 +9,8 @@ router.post('/login', login);
 // product APIs 
 router.post("/createProduct", createProduct);
 router.get("/getProduct", getProducts);
+router.get("/product/:id", getProductById);
+router.delete("/product/:id", deleteProduct);
+router.put("/product/:id", updateProduct);
 
 module.exports = router;
